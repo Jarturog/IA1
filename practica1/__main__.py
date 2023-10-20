404be87ce499b50e50bb4c40aadee7818365f91a
Arturo <69714460+Jarturog@users.noreply.github.com>
2023-10-20 20:37:44 +0200

    platform

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -1,4 +1,10 @@
+import sys, platform
 from practica1 import agent, joc
+P = platform.system()
+if P == 'Windows': sys.path.append('C:\\Users\\jartu\\Documents\\GitHub\\IA1') # path de arturo
+elif P == 'Darwin': pass#sys.path.append('C:\\Users\\jartu\\Documents\\GitHub\\IA1') # path de marta
+
+
 
 
 def main():
