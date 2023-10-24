7a43138adeacb82ef7fadfedd9d56e25c79d71a4
Arturo <69714460+Jarturog@users.noreply.github.com>
2023-10-24 10:53:43 +0200

    casilla jugador

--- practica1/agent.py
+++ practica1/agent.py
@@ -9,7 +9,6 @@ from ia_2022 import entorn
 from practica1 import joc
 from practica1.entorn import Accio, SENSOR, TipusCasella # no sé si TipusCasella se puede importar
 
-CASELLA_JUGADOR = TipusCasella.CARA
 N_CASELLAS_PER_GUANYAR = 4
 
 class Agent(joc.Agent):
@@ -74,7 +73,7 @@ class Estat:
             casella = taulell[i][j]
             if casella == TipusCasella.LLIURE:
                 h_casilla += 1
-            elif casella != CASELLA_JUGADOR: # per tant és casella perdedora
+            elif casella != self.jugador: # per tant és casella perdedora
                 h_casilla += 2
 
             direcciones = [(di, dj) for di in [-1, 0, 1] for dj in [-1, 0, 1] if di != 0 or dj != 0]
@@ -87,7 +86,7 @@ class Estat:
                     casella = taulell[ind1][ind2]
                     if casella == TipusCasella.LLIURE:
                         h_casilla += 1
-                    elif casella != CASELLA_JUGADOR: # per tant és casella perdedora
+                    elif casella != self.jugador: # per tant és casella perdedora
                         h_casilla += 2
             return h_casilla
 
@@ -112,7 +111,7 @@ class Estat:
         taulell = self.taulell
         def check_direccio(i, j, di, dj):
             for k in range(N_CASELLAS_PER_GUANYAR):
-                if taulell[i + (k * di)][j + (k * dj)] != CASELLA_JUGADOR:
+                if taulell[i + (k * di)][j + (k * dj)] != self.jugador:
                     return False
             return True
         # Iterar a través de todas las celdas de la matriz
@@ -120,7 +119,7 @@ class Estat:
         for i in range(filas):
             columnas = len(taulell[i])
             for j in range(columnas):
-                if taulell[i][j] != CASELLA_JUGADOR:
+                if taulell[i][j] != self.jugador:
                     continue
                 # Verificar las cuatro direcciones posibles: horizontal, vertical, diagonal descendente y diagonal ascendente
                 for di, dj in [(0, 1), (1, 0), (1, 1), (1, -1)]:
@@ -148,7 +147,7 @@ class Estat:
                 if self.taulell[i][j] != TipusCasella.LLIURE:
                     continue
                 taulell = [fila[:] for fila in self.taulell] # copia de valores, no de referencia
-                taulell[i][j] = CASELLA_JUGADOR #.posar(TIPUS_CASELLA_JUGANT)
+                taulell[i][j] = self.jugador #.posar(TIPUS_CASELLA_JUGANT)
                 acc = self.accions_previes[:]
                 acc.append((Accio.POSAR, (i, j)))
                 nou_estat = Estat(self.mida, taulell, acc)
