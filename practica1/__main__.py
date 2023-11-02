f1ed7102dcec655501ae0a45bab7483c81817976
Arturo <69714460+Jarturog@users.noreply.github.com>
2023-11-02 22:51:21 +0100

    (8, 8)

--- practica1/__main__.py
+++ practica1/__main__.py
@@ -9,7 +9,7 @@ def main():
     #agents = [AgentAestrella("Aestrella")]
     #agents = [AgentMiniMaxAlfaBeta("Jugador 1", 2), AgentMiniMaxAlfaBeta("Jugador 2", 2)]
     #agents = [AgentMiniMaxAlfaBeta("Jugador 1 iteratiu", 2, True), AgentMiniMaxAlfaBeta("Jugador 2 iteratiu", 2, True)]
-    quatre = joc.Taulell(agents, (5, 5))
+    quatre = joc.Taulell(agents)
     quatre.comencar()
 
 if __name__ == "__main__":
